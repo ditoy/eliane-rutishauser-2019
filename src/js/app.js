ae163c4b853aa170a16cc3032c36c1283c91bb3f
far-ditoy <alipur@ditoy.com>
2018-07-24 11:20:07 +0200

    Fine tuning slideshow home

--- src/js/app.js
+++ src/js/app.js
@@ -402,3 +402,25 @@ if (!!customersList) {
 
 
 
+
+/**
+ * Home Slideshow
+ */
+var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,8000);
+
+function nextSlide(){
+	slides[currentSlide].className = 'slide';
+	currentSlide = (currentSlide+1)%slides.length;
+	slides[currentSlide].className = 'slide showing';
+}
+
+
+
+
+
+
+
+
+
