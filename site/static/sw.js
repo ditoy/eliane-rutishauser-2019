14e163b2376b7206f48abed8747d95a33e5acc44
Dietrich Rordorf <dr@ediqo.com>
2018-11-29 17:28:14 +0100

    cache busting

--- site/static/sw.js
+++ site/static/sw.js
@@ -3,7 +3,7 @@
  * @see https://raw.githubusercontent.com/wildhaber/offline-first-sw/master/sw.js
  */
 
-const CACHE_VERSION = 2;
+const CACHE_VERSION = 10;
 
 const BASE_CACHE_FILES = [
     '/css/main.css',
